b6cdd10961b9319ba6f8da22a32ef11525997cb5
gaoke <g15339078296@163.com>
2019-11-12 21:57:45 +0800

    1

---
+++ src/main/java/com/example/shirodemo/service/impl/UserServiceimpl.java
@@ -0,0 +1,16 @@
+package com.example.shirodemo.service.impl;
+
+import com.example.shirodemo.model.User;
+import com.example.shirodemo.service.UserService;
+
+public class UserServiceimpl implements UserService {
+    @Override
+    public User findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public User findById(Integer id) {
+        return null;
+    }
+}
